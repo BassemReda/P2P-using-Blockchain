363c8425a7b77bc0bee6b1bb72c8d021c833b83f
Bassem Reda <34349984+BassemReda@users.noreply.github.com>
2019-03-09 19:24:54 +0200

    added constructor and attributes to P2PConnection class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,20 @@
 package com.company;
 
-import de.tum.in.www1.jReto.Connection;
-import de.tum.in.www1.jReto.LocalPeer;
-import de.tum.in.www1.jReto.module.wlan.WlanModule;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.concurrent.Executors;
 
 public class Main {
 
     public static void main(String args[])
     {
-        System.out.println(Hash.getSHA256("omar"));
-        try {
-            WlanModule wlanModule = new WlanModule("ExampleType");
-            LocalPeer localPeer = new LocalPeer(Arrays.asList(wlanModule), Executors.newSingleThreadExecutor());
-            localPeer.start(
-                    discoveredPeer -> System.out.println("Discovered peer: "+discoveredPeer),
-                    removedPeer -> System.out.println("Removed peer: "+removedPeer),
-                    (peer, incomingConnection) -> System.out.println("Received incoming connection: "+incomingConnection+" from peer: "+peer)
-            );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            WlanModule wlanModule = new WlanModule("ExampleType");
+//            LocalPeer localPeer = new LocalPeer(Arrays.asList(wlanModule), Executors.newSingleThreadExecutor());
+//            localPeer.start(
+//                    discoveredPeer -> System.out.println("Discovered peer: "+discoveredPeer),
+//                    removedPeer -> System.out.println("Removed peer: "+removedPeer),
+//                    (peer, incomingConnection) -> System.out.println("Received incoming connection: "+incomingConnection+" from peer: "+peer)
+//            );
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 }
